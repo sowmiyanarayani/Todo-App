e2221b159d0512060e977a508f449639b9697f05
sowmiyanarayani <sowmiya2692001@gmail.com>
2024-08-31 11:08:50 +0530

    Commit the App.js.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { React } from 'react';
 import './App.scss';
+import OutputTodo from './components/OutputTodo';
 
-const App = () =>
-	<div className="App">
-		Ready to start.
+const App = (context) =>
+	<div className="todoContainer">
+		<OutputTodo { ...context }/>
 	</div>;
 
 export default App;
