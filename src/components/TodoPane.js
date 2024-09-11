d5d8d779a5a41fc7bdd9c412fd34264d93ea18ce
sowmiyanarayani <sowmiya2692001@gmail.com>
2024-09-11 21:23:42 +0530

    Final display.

--- src/components/TodoPane.js
+++ src/components/TodoPane.js
@@ -2,15 +2,18 @@ import { React } from 'react';
 import InputTodo from './InputTodo';
 import Todo from './Todo';
 import AddTodo from './AddTodo';
+import ToggleAllButton from './ToggleAllButton';
+import ClearButton from './ClearButton';
 
 const TodoPane = (context) => <div>
 	<h1>Todo App</h1>
 	<div>
+		<ToggleAllButton { ...context }/>
 		<InputTodo { ...context }/>
 		<AddTodo { ...context }/>
-
 	</div>
 	<Todo { ...context }/>
+	{ ClearButton() }
 </div>;
 
 export default TodoPane;
