e2acb69d11670ac758a9795defb5c567cf9abc02
sowmiyanarayani <sowmiya2692001@gmail.com>
2024-09-11 21:24:20 +0530

    toggleall button.

---
+++ src/components/ActionButton.js
@@ -0,0 +1,11 @@
+/* eslint-disable react/react-in-jsx-scope */
+import context from '../core/context';
+import AddTodo from './AddTodo.js';
+import EditButton from './EditButton.js';
+
+const ActionButton = () => (context.state.editing
+	? <EditButton/>
+	: <AddTodo/>
+);
+
+export default ActionButton;
