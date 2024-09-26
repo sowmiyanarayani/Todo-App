88da3c2ba8d2c58fd5d6b98d65b7a77ef1d8bdf2
sowmiyanarayani <sowmiya2692001@gmail.com>
2024-09-26 23:57:56 +0530

    toggle all button.

--- src/components/ToggleAllButton.js
+++ src/components/ToggleAllButton.js
@@ -1,12 +1,10 @@
 /* eslint-disable no-mixed-spaces-and-tabs */
 /* eslint-disable react/jsx-indent-props */
 import { React } from 'react';
+import context from '../core/context';
 import TodoManager from '../services/TodoManager';
-const ToggleAllButton = (context) => {
-	const {
-		state: { todos },
-		actions: { toggleTodos },
-	} = context;
+const ToggleAllButton = () => {
+	const { state: { todos }, actions: { toggleTodos }} = context;
 	const isChecked = TodoManager.getActiveCount(todos) === 0 ;
 	const noTodos = TodoManager.hasNoTodos(todos) ;
 
