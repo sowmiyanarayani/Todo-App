34429620a253c2cb6054bc4ae665f1ee17e154b7
sowmiyanarayani <sowmiya2692001@gmail.com>
2024-09-26 23:57:17 +0530

    Todo manager.

--- src/services/TodoManager.js
+++ src/services/TodoManager.js
@@ -1,72 +1,55 @@
 import { rndString } from '@laufire/utils/random';
 import config from '../core/config';
-import { peek } from '@laufire/utils/debug';
 const { idLength } = config;
-const filters = {
-	all: () => true,
-	active: (todo) => !todo.completed,
-	completed: (todo) => todo.completed,
-};
 
-const getTodo = (text) => {
-	peek(text);
-	return {
-		id: rndString(idLength),
-		text: text,
-		completed: false,
-	};
-};
+const getTodo = (text) => ({
+	id: rndString(idLength),
+	text: text,
+	completed: false,
+});
 
 const addText = (todos, text) =>
 	(text === '' ? todos : [...todos, getTodo(text)]);
 
-const deleteTodo = (context) => {
-	const { state: { todos }, data } = context;
-
-	peek(todos);
-	return todos.filter((select) => select.id !== data);
-};
+const deleteTodo = ({ state: { todos }, data }) =>
+	todos.filter((select) => select.id !== data.id);
 
 const editTodo = (
-	todos, editing, text
+	todos, editingTodo, text
 ) =>
-	todos.map((todo) => (todo.id !== editing?.id
+	todos.map((todo) => (todo.id !== editingTodo?.id
 		? todo
-		: {
-			...todo,
-			text,
-		}));
+		: { ...todo, text }));
 
-const toggleCompletion = (context) => {
-	const { state: { todos }, data: id } = context;
-
-	return todos.map((todo) => (todo.id !== id
+const toggleCompletion = ({ state: { todos }, data }) =>
+	todos.map((todo) => (todo.id !== data.id
 		? todo
-		: {
-			...todo,
-			completed: !todo.completed,
-		}));
-};
+		: { ...todo, completed: !todo.completed }));
 
-const toggleTodos = (todos, isComplete) => todos.map((todo) => ({
-	...todo,
-	completed: isComplete,
-}));
+const toggleTodos = (todos, isComplete) =>
+	todos.map((todo) => ({ ...todo, completed: isComplete }));
 
 const getActiveCount = (todos) =>
-	todos.filter(filters.active).length;
+	todos.filter((todo) => !todo.completed).length;
 
 const getAllCount = (todos) => todos.length;
 
-const clearCompleted = (todos) => todos.filter(filters.active);
+const getClearCount = (todos) =>
+	todos.filter((todo) => todo.completed).length;
 
-const getClearCount = (todos) => todos.filter(filters.completed).length;
+const clearCompleted = (todos) =>
+	todos.filter((todo) => !todo.completed);
 
 const getTodoCount = (todos) => todos.length;
 
-const filterTodo = (todos, filter) => {
-	peek(filter);
-	return todos.filter(filters[filter]);
+const filterTodo = (context) => {
+	const filters = {
+		all: () => true,
+		active: (todo) => !todo.completed,
+		completed: (todo) => todo.completed,
+	};
+
+	return context.state.todos.filter(filters[context.state.filter]);
 };
 
 const hasNoTodos = (todos) => getTodoCount(todos) === 0;
