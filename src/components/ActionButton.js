34b7f64cc1b78813ae958c9ab06aaed4cc907c3a
sowmiyanarayani <sowmiya2692001@gmail.com>
2024-09-27 16:07:56 +0530

    Implement ActionButton component - Implement conditional rendering for Add and Edit Button.

--- src/components/ActionButton.js
+++ src/components/ActionButton.js
@@ -2,9 +2,10 @@ import context from '../core/context';
 import AddTodo from './AddTodo.js';
 import EditButton from './EditButton.js';
 
-const ActionButton = () => (context.state.editing
-	? EditButton()
-	: AddTodo()
-);
+const ActionButton = () => {
+	const { state: { editing }} = context;
+
+	return editing ? EditButton() : AddTodo();
+};
 
 export default ActionButton;
