43f3cd8bbcc79a0f2909a3a2c600d7b0dea0ee72
sowmiyanarayani <sowmiya2692001@gmail.com>
2024-09-27 16:20:59 +0530

    Add Todo component - Add checkbox for completion,editing,and deleting.

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,25 +1,27 @@
-/* eslint-disable max-len */
 /* eslint-disable max-lines-per-function */
-
 import { React } from 'react';
 import context from '../core/context';
 
 const Todo = ({ id, text, completed }) =>
-	<div key={ id }>
+	<div key={ id } className="todo">
 		<span>
 			<input
 				type="checkbox"
 				checked={ completed }
-				onChange={ () => { context.actions.toggleCompletion({ id, text, completed }); } }
+				onChange={ () => {
+					context.actions.toggleCompletion({ id, text, completed });
+				} }
 			/>
 		</span>
 		<span
-			onClick={ () => context.actions.setEditing({ id, text, completed }) }
-		>
-			{ text }</span>
+			onClick={ () =>
+				context.actions.setEditing({ id, text, completed }) }
+		>{ text }
+		</span>
 		<span>
-			<button onClick={ () => context.actions.deleteTodo({ id, text, completed }) }> X
-			</button>
+			<button onClick={ () =>
+				context.actions.deleteTodo({ id, text, completed }) }
+			> X </button>
 		</span>
 	</div>;
 
