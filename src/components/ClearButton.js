44abeed82d30accb96c32c634cec417483cc0313
sowmiyanarayani <sowmiya2692001@gmail.com>
2024-09-28 20:11:21 +0530

    Add clear Button component: Implement the functionality to clear completed tasks.

--- src/components/ClearButton.js
+++ src/components/ClearButton.js
@@ -1,6 +1,7 @@
 import { React } from 'react';
 import TodoManager from '../services/TodoManager';
 import context from '../core/context';
+
 const ClearButton = () => {
 	const { state: { todos }, actions: { clearCompleted }} = context;
 	const hasNOTodos = TodoManager.hasNoTodos(todos);
