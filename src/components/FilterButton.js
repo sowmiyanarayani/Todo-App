39d459eba0244a55bd6975100273212eeca09aa2
sowmiyanarayani <sowmiya2692001@gmail.com>
2024-09-28 20:12:18 +0530

    Add FilterBar and FilterButton - Implement UI for Filtering Todos.

--- src/components/FilterButton.js
+++ src/components/FilterButton.js
@@ -3,16 +3,12 @@ import context from '../core/context';
 import TodoManager from '../services/TodoManager.js';
 
 const FilterButton = ({ filter }) => {
-	const {
-		state: { todos },
-		actions: { setFilter },
-	} = context;
-
+	const { state: { todos }, actions: { setFilter }} = context;
 	const noTodos = TodoManager.getTodoCount(todos) === 0;
 
 	return noTodos
 		? null
-		: <button key={ filter } onClick={ () => setFilter(filter) }>
+		: <button onClick={ () => setFilter(filter) }>
 			{ filter }
 		</button>;
 };
