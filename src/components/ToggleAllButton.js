9c9c09008612aab10cb6d88f2f70318609cce960
sowmiyanarayani <sowmiya2692001@gmail.com>
2024-09-28 14:30:56 +0530

    toggleAllButton Component- implement toggle all functionality to mark todos as complete or incomplete.

--- src/components/ToggleAllButton.js
+++ src/components/ToggleAllButton.js
@@ -1,18 +1,20 @@
 /* eslint-disable no-mixed-spaces-and-tabs */
 /* eslint-disable react/jsx-indent-props */
-import { React } from 'react';
+import React from 'react';
 import context from '../core/context';
 import TodoManager from '../services/TodoManager';
+
 const ToggleAllButton = () => {
 	const { state: { todos }, actions: { toggleTodos }} = context;
-	const isChecked = TodoManager.getActiveCount(todos) === 0 ;
-	const noTodos = TodoManager.hasNoTodos(todos) ;
+
+	const isChecked = TodoManager.getActiveCount(context) === 0;
+	const noTodos = TodoManager.hasNoTodos(todos);
 
 	return noTodos
 		? null
 		: <input
 			type="checkbox"
-			checked={	isChecked	}
+			checked={ isChecked }
 			onChange={ () => toggleTodos(!isChecked) }
 		  />;
 };
