4bbbe2ac459b55a4366e9f24302f28eeefdb14b0
sowmiyanarayani <sowmiya2692001@gmail.com>
2024-10-19 12:38:29 +0530

    Initial Commit - Tasklist component with predefined todo.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import { React } from 'react';
+/* eslint-disable react/jsx-indent */
+/* eslint-disable indent */
+import { React, useEffect } from 'react';
 import './App.scss';
 import TodoPane from './components/TodoPane';
+import TaskPane from './components/TaskPane';
+import Ticker from './services/Ticker';
 
-const App = (context) =>
-	<div>
-		<TodoPane { ...context }/>
-	</div>;
+const App = (context) => {
+ useEffect(Ticker, []);
+
+return <div>
+  <TodoPane { ...context }/>
+	<TaskPane { ...context }/>
+</div>;
+};
 
 export default App;
