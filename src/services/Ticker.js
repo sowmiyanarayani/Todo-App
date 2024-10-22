8ce07c26ff310a772a1ff9e4f081e6d2b36f6f5e
sowmiyanarayani <sowmiya2692001@gmail.com>
2024-10-22 13:05:32 +0530

    Ticker - Generate the random words every 5s.

--- src/services/Ticker.js
+++ src/services/Ticker.js
@@ -1,19 +1,18 @@
-/* eslint-disable padding-line-between-statements */
-/* eslint-disable arrow-body-style */
 /* eslint-disable indent */
+
 import context from '../core/context';
 
 const Ticker = () => {
 	const start = () => {
-		const { actions, config: { tickerDelay, words }} = context;
+		const { actions: { addTask }, config: { tickerDelay, words }} = context;
 
-		const getRandomWord = () => {
-						return words[Math.floor(Math.random() * words.length)];
-		};
+		const getRandomWord = () =>
+		words[Math.floor(Math.random() * words.length)];
 
 		setInterval(() => {
 			const randomWord = getRandomWord();
-		actions.addTask(randomWord);
+
+   addTask(randomWord);
 		}, tickerDelay);
 	};
 
